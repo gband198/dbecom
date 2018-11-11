20cf0efb83cbd8c42eac79610ece69c75a45b2da
gband198 <ancadumo@gmail.com>
2018-11-11 18:00:05 -0500

    nuevo cambio de permisos

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Proyect
     config.middleware.insert_before 0, Rack: :cors do
         allow do 
             origins '*'
-            resource '*' :headers => :any, :methods => [:get, :post, :options, :put, :delete]
+            resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
         end
     end
     # Settings in config/environments/* take precedence over those specified here.
